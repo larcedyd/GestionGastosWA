18ccffe0f5f19e916b387cc58da5189b8da05442
Leo220617 <32038650+Leo220617@users.noreply.github.com>
2022-08-25 16:11:31 -0600

    cambios

--- CheckIn.API/Controllers/ProveedoresController.cs
+++ CheckIn.API/Controllers/ProveedoresController.cs
@@ -62,6 +62,166 @@ namespace CheckIn.API.Controllers
             }
             catch (Exception ex)
             {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StackTrace = ex.StackTrace;
+                be.Metodo = "GET ALL PROVEEDOR";
+                be.Fecha = DateTime.Now;
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+                G.CerrarConexionAPP(db);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [Route("api/Proveedores/Consultar")]
+        public HttpResponseMessage GetOne([FromUri]int id)
+        {
+            try
+            {
+                G.AbrirConexionAPP(out db);
+
+
+                var Proveedor = db.Proveedores.Where(a => a.id == id).FirstOrDefault();
+
+
+                if (Proveedor == null)
+                {
+                    throw new Exception("Esta Proveedor no se encuentra registrada");
+                }
+                G.CerrarConexionAPP(db);
+                return Request.CreateResponse(HttpStatusCode.OK, Proveedor);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StackTrace = ex.StackTrace;
+                be.Metodo = "GET ONE PROVEEDOR";
+                be.Fecha = DateTime.Now;
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+                G.CerrarConexionAPP(db);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] Proveedores proveedor)
+        {
+            try
+            {
+                G.AbrirConexionAPP(out db);
+
+                var Proveedor = db.Proveedores.Where(a => a.id == proveedor.id).FirstOrDefault();
+
+                if (Proveedor == null)
+                {
+                    Proveedor = new Proveedores();
+                    Proveedor.RUC = proveedor.RUC;
+                    Proveedor.DV = proveedor.DV;
+                    Proveedor.Nombre = proveedor.Nombre;
+
+                    db.Proveedores.Add(Proveedor);
+                    db.SaveChanges();
+
+                }
+                else
+                {
+                    throw new Exception("Esta Proveedor YA existe");
+                }
+
+                G.CerrarConexionAPP(db);
+                return Request.CreateResponse(HttpStatusCode.OK, Proveedor);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StackTrace = ex.StackTrace;
+                be.Metodo = "Insercion de Proveedor";
+                be.Fecha = DateTime.Now;
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+                G.CerrarConexionAPP(db);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpPut]
+        [Route("api/Proveedores/Actualizar")]
+        public HttpResponseMessage Put([FromBody] Proveedores proveedor)
+        {
+            try
+            {
+                G.AbrirConexionAPP(out db);
+
+                var Proveedor = db.Proveedores.Where(a => a.id == proveedor.id).FirstOrDefault();
+
+                if (Proveedor != null)
+                {
+                    db.Entry(Proveedor).State = EntityState.Modified;
+                    Proveedor.RUC = proveedor.RUC;
+                    Proveedor.DV = proveedor.DV;
+                    Proveedor.Nombre = proveedor.Nombre;
+                    db.SaveChanges();
+
+                }
+                else
+                {
+                    throw new Exception("Proveedor no existe");
+                }
+                G.CerrarConexionAPP(db);
+                return Request.CreateResponse(HttpStatusCode.OK, Proveedor);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StackTrace = ex.StackTrace;
+                be.Metodo = "Actualizar Proveedor";
+                be.Fecha = DateTime.Now;
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
+                G.CerrarConexionAPP(db);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/Proveedores/Eliminar")]
+        public HttpResponseMessage Delete([FromUri] int id)
+        {
+            try
+            {
+                G.AbrirConexionAPP(out db);
+
+                var Proveedor = db.Proveedores.Where(a => a.id == id).FirstOrDefault();
+
+                if (Proveedor != null)
+                {
+
+
+                    db.Proveedores.Remove(Proveedor);
+                    db.SaveChanges();
+
+                }
+                else
+                {
+                    throw new Exception("Proveedor no existe");
+                }
+                G.CerrarConexionAPP(db);
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                BitacoraErrores be = new BitacoraErrores();
+                be.Descripcion = ex.Message;
+                be.StackTrace = ex.StackTrace;
+                be.Metodo = "Eliminar Proveedor";
+                be.Fecha = DateTime.Now;
+                db.BitacoraErrores.Add(be);
+                db.SaveChanges();
                 G.CerrarConexionAPP(db);
                 return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
             }
