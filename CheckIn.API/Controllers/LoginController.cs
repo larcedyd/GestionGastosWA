dc8fe3ef4cf08c867d5d70dc172adb4138ce01ce
Leo220617 <32038650+Leo220617@users.noreply.github.com>
2025-07-21 13:38:09 -0600

    ca

--- CheckIn.API/Controllers/LoginController.cs
+++ CheckIn.API/Controllers/LoginController.cs
@@ -114,7 +114,7 @@ namespace CheckIn.API.Controllers
                     user.Contador += 1;
                     if (user.Contador > 3)
                     {
-                        user.Activo = false;
+
                         var BD2 = Licencia.CadenaConexionBD;
 
                         db = new ModelCliente(BD2);
@@ -133,6 +133,9 @@ namespace CheckIn.API.Controllers
                         dbLogin.Entry(LicenciaUsuarios).State = EntityState.Modified;
                         LicenciaUsuarios.Activo = false;
                         dbLogin.SaveChanges();
+                        db.Entry(user).State = EntityState.Modified;
+                        user.Activo = false;
+                        db.SaveChanges();
                     }
 
                     if (!user.Activo.Value)
@@ -147,7 +150,6 @@ namespace CheckIn.API.Controllers
                     bmx.Fecha = DateTime.Now;
                     db.LogsUsuarios.Add(bmx);
                     db.SaveChanges();
-                    throw new Exception("Clave o Usuario incorrectos");
                     throw new Exception("Clave o Usuario incorrectos");
                 }
                 else
@@ -175,6 +177,114 @@ namespace CheckIn.API.Controllers
                 de.UrlLogo = param.UrlImagenesApp + param.UrlLogo;
                 de.CambiarClave = user.FechaVencimientoClave.Date <= DateTime.Now.Date ? true : user.CambiarClave;
                 de.Pais = Licencia.CadenaConexionSAP;
+                de.QAD = Licencia.QAD ? "SI" : "NO";
+                de.Codigo = user.Codigo;
+                BitacoraLogin bl = new BitacoraLogin();
+                bl.idUsuario = de.idLogin;
+                bl.IP = HttpContext.Current.Request.UserHostAddress;
+                bl.Detalle = "El usuario " + de.NombreUsuario + ", con el id: " + de.idLogin + " se ha logueado";
+                bl.Fecha = DateTime.Now;
+                db.BitacoraLogin.Add(bl);
+                db.SaveChanges();
+
+                if (SeguridadModulos.Where(a => a.CodModulo == 99).FirstOrDefault() != null)
+                {
+                    metodos met = new metodos();
+                    CorreoEnvio correo = db.CorreoEnvio.FirstOrDefault();
+                    if (!met.EnviarCorreo(user.Email, user.Codigo, correo))
+                    {
+                        G.GuardarTxt("ErrorEnviandoEmail.txt", user.Codigo + " => " + user.Email);
+                    }
+                }
+
+
+
+                return Request.CreateResponse(HttpStatusCode.OK, de);
+
+            }
+            catch (Exception ex)
+            {
+                //BitacoraErrores be = new BitacoraErrores();
+                //be.Descripcion = ex.Message;
+                //be.StackTrace = ex.StackTrace;
+                //be.Metodo = "LOGIN de Usuario";
+                //be.Fecha = DateTime.Now;
+                //db.BitacoraErrores.Add(be);
+                //db.SaveChanges();
+                //G.CerrarConexionAPP(db);
+                G.GuardarTxt("ErrorLogin.txt", ex.Message + " => " + ex.StackTrace);
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        [Route("api/Login/Conectar365")]
+        public async Task<HttpResponseMessage> GetLogin365Async([FromUri] string email, string CedulaJuridica = "")
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(CedulaJuridica))
+                {
+                    throw new Exception("Se debe indicar el número de compañía a la que perteneces");
+                }
+
+                var LicenciaUsuarios = dbLogin.LicUsuarios.Where(a => a.Email.ToUpper().Contains(email.ToUpper()) && a.CedulaJuridica == CedulaJuridica).FirstOrDefault();
+
+
+                if (LicenciaUsuarios == null)
+                {
+                    throw new Exception("Usuario no existe");
+                }
+                var Licencia = dbLogin.LicEmpresas.Where(a => a.CedulaJuridica == LicenciaUsuarios.CedulaJuridica).FirstOrDefault();
+
+                if (Licencia == null)
+                {
+                    throw new Exception("Empresa no existe");
+                }
+
+                if (!Licencia.Activo.Value)
+                {
+                    throw new Exception("Empresa no se encuentra activa");
+                }
+
+                if (!LicenciaUsuarios.Activo)
+                {
+                    throw new Exception("Este usuario no esta activo");
+                }
+
+
+
+
+                var BD = Licencia.CadenaConexionBD;
+
+                db = new ModelCliente(BD);
+                var token = TokenGenerator.GenerateTokenJwt(Licencia.CedulaJuridica, BD, Licencia.QAD == true ? "SI" : "NO");
+
+                DevolucionLogin de = new DevolucionLogin();
+                var user = db.Login.Where(a => a.Email.ToUpper().Contains(LicenciaUsuarios.Email.ToUpper())).FirstOrDefault();
+
+                if (user == null)
+                {
+                    throw new Exception("Usuario no existe");
+                }
+
+                var SeguridadModulos = db.SeguridadRolesModulos.Where(a => a.CodRol == user.idRol).ToList();
+                var param = db.Parametros.FirstOrDefault();
+
+                db.Entry(user).State = EntityState.Modified;
+                user.Codigo = G.GenerarCodigo();
+                db.SaveChanges();
+
+                de.idLogin = user.id;
+                de.NombreUsuario = LicenciaUsuarios.Nombre;
+                de.Email = LicenciaUsuarios.Email;
+                de.CedulaJuridica = LicenciaUsuarios.CedulaJuridica;
+                de.FechaVencimiento = Licencia.FechaVencimiento.Value;
+                de.token = token;
+                de.idRol = user.idRol.Value;
+                de.Seguridad = SeguridadModulos;
+                de.UrlLogo = param.UrlImagenesApp + param.UrlLogo;
+                de.CambiarClave = user.FechaVencimientoClave.Date <= DateTime.Now.Date ? true : user.CambiarClave;
+                de.Pais = Licencia.CadenaConexionSAP;
+                de.Codigo = user.Codigo;
                 de.QAD = Licencia.QAD ? "SI" : "NO";
                 BitacoraLogin bl = new BitacoraLogin();
                 bl.idUsuario = de.idLogin;
@@ -213,7 +323,6 @@ namespace CheckIn.API.Controllers
                 return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
             }
         }
-
         public async Task<HttpResponseMessage> Get([FromUri] Filtros filtro)
         {
             try
@@ -625,6 +734,7 @@ namespace CheckIn.API.Controllers
         public string token { get; set; }
         public string UrlLogo { get; set; }
         public bool CambiarClave { get; set; }
+        public string Codigo { get; set; }
         public string Pais { get; set; }
         public string QAD { get; set; }
         public List<SeguridadRolesModulos> Seguridad { get; set; }
